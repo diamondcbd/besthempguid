f1d6152cb40311ba21c6ca7250ee08d01f9a1793
First Cap Ven <68794110+firstcapventure@users.noreply.github.com>
2020-08-22 14:36:59 -0400

    Add files via upload

---
+++ js/doc.js
@@ -0,0 +1,101 @@
+document.getElementById("first-el").innerHTML = "With so many different CBD brands available it can be difficult to navigate which company and products are right for you. As experts in this field we have tested over 1000 CBD products to help you make the right choice! Best Hemp Guide has gathered over 20K real customer reviews from across the web, to provide you the most accurate and comprehensive analysis of each brand.";
+document.getElementById("box-1").innerHTML = "Expert advice and recommendations from real CBD users!";
+document.getElementById("box-2").innerHTML = "Regular Updates - We're regularly updating our reviews and rankings!";
+document.getElementById("box-3").innerHTML = "100% Independent! Brands can’t pay for reviews or placement!";
+
+
+
+document.getElementById("title-1").innerHTML = "Diamond CBD";
+document.getElementById("text-1").innerHTML = "A true leader in the CBD industry and one of the very first brands, Diamond CBD has been producing high quality hemp and CBD products for over 7 years. Diamond CBD’s, main focus is to research and develop innovative CBD hemp extracts and to make them accessible worldwide.";
+document.getElementById("title1-1").innerHTML = "Pros";
+document.getElementById("li-1D").innerHTML = "55% off coupon";
+document.getElementById("li-2D").innerHTML = "Free N95 masks and hand sanitizer with every purchase.";
+document.getElementById("li-3D").innerHTML = "Fast & Free 2-day shipping.";
+document.getElementById("li-4D").innerHTML = "Over 100 products to choose from.";
+document.getElementById("li-5D").innerHTML = "Full spectrum, broad spectrum and isolate options.";
+document.getElementById("li-6D").innerHTML = "Great Value! The lowest prices in the industry for quality CBD!";
+
+
+
+document.getElementById("title-2").innerHTML = "Meds Biotech";
+document.getElementById("text-2").innerHTML = "Meds Biotech is one of America's leading makers of elite quality CBD oil and CBD products geared towards medicinal use. Crafted for the medical industry, Meds Biotech manufactures high-quality pharmacist-formulated CBD products. With a dedicated team of doctors, advisors, and medical staff, their primary goal is the ensure access to CBD that meets premium efficacy standards.";
+document.getElementById("title2-1").innerHTML = "Pros";
+document.getElementById("li-1M").innerHTML = "55% off coupon";
+document.getElementById("li-2M").innerHTML = "Fast & Free 2-day shipping.";
+document.getElementById("li-3M").innerHTML = "A wider range of CBD strengths.";
+document.getElementById("li-4M").innerHTML = "CBD capsules for accurate dosing.";
+document.getElementById("li-5M").innerHTML = "Child safe packaging.";
+document.getElementById("li-6M").innerHTML = "Generous 30-day return policy.";
+
+
+
+document.getElementById("title-3").innerHTML = "Green Roads";
+document.getElementById("text-3").innerHTML = "Green Roads is a pharmacist-founded hemp company that has set the gold standard for quality since the earliest days of the industry. Green Roads CBD Oil is one of the most popular choices among CBD users. With a concentration ranging from 350mg to 3500mg, it's a good choice for both beginners and those wanting to increase their dosage.";
+document.getElementById("title3-1").innerHTML = "Pros";
+document.getElementById("li-1G").innerHTML = "CBD line created by a pharmacist.";
+document.getElementById("li-2G").innerHTML = "A large variety of products and price points.";
+document.getElementById("li-3G").innerHTML = "One of the largest and most respected CBD brands.";
+document.getElementById("li-4G").innerHTML = "All products are tested by third-party laboratories.";
+document.getElementById("li-5G").innerHTML = "High quality products";
+document.getElementById("li-6G").innerHTML = "Transparent lab reports with QR codes.";
+
+
+
+
+document.getElementById("title-4").innerHTML = "Medterra";
+document.getElementById("text-4").innerHTML = "Started by a group of individuals passionate about the power of HEMP, Medterra believes that HEMP should be available to all of those who search for high quality HEMP at an affordable price. Their hemp-derived products are designed to create trusted wellness products that are continually purchased, loved and recommended throughout the United States.";
+document.getElementById("title4-1").innerHTML = "Pros";
+document.getElementById("li-md1").innerHTML = "Inexpensive compared to some comparable brands and products.";
+document.getElementById("li-md2").innerHTML = "Non-GMO, organic, no-pesticide CBD sourced from hemp grown in the U.S.";
+document.getElementById("li-md3").innerHTML = "Unique products for morning, evening, and female wellness.";
+document.getElementById("li-md4").innerHTML = "Generous 30-day return policy.";
+document.getElementById("li-md5").innerHTML = "Varying concentrations.";
+document.getElementById("li-md6").innerHTML = "Third-party testing results available for the public";
+
+
+
+
+document.getElementById("title-5").innerHTML = "Charlottes Web";
+document.getElementById("text-5").innerHTML = "From the beginning, Charlotte’s Web has demonstrated a dedication to quality unmatched in the CBD world with top-tier hemp specifically bred for its high CBD and low THC content. This company helped start the CBD industry and continues to educate and promote the value and benefits of CBD.";
+document.getElementById("title5-1").innerHTML = "Pros";
+document.getElementById("li-c1").innerHTML = "Medical-grade CBD tinctures, isolates, capsules, topicals, and dog oils.";
+document.getElementById("li-c2").innerHTML = "Streamlined online store experience.";
+document.getElementById("li-c3").innerHTML = "American-sourced hemp.";
+document.getElementById("li-c4").innerHTML = "Wide range of products available";
+document.getElementById("li-c5").innerHTML = "Mission-driven organization that supports cannabis science research & medicine";
+document.getElementById("li-c6").innerHTML = "One of the more robust batch testing operations we’ve come across.";
+
+
+
+document.getElementById("title-6").innerHTML = "Just CBD";
+document.getElementById("text-6").innerHTML = "JustCBD is a relatively new company, started in 2017, but nonetheless remains a quality and well-respected name in the industry. JustCBD was founded on the basis that Cannabidiol is Mother Nature’s secret miracle. With the help of world-class labs to test their products, JustCBD is made with industry-leading quality, honesty, and love. ";
+document.getElementById("title6-1").innerHTML = "Pros";
+document.getElementById("li-j1").innerHTML = "Large variety of CBD gummies and edibles";
+document.getElementById("li-j2").innerHTML = "Wide range of oil flavors and strengths.";
+document.getElementById("li-j3").innerHTML = "Great value one of the cheaper brands available";
+document.getElementById("li-j4").innerHTML = "Hassle free Money-Back Guarantee, Free returns.";
+document.getElementById("li-j5").innerHTML = "Free US Shipping for orders above $20";
+
+
+
+document.getElementById("title-7").innerHTML = "Fab CBD";
+document.getElementById("text-7").innerHTML = "FAB CBD was founded in 2017 with the goal of building a product line for the everyday person to feel comfortable supplementing with hemp. They believe in formulating quality products with a specific intent and purpose. No gimmicks, no hype, just results. Fab CBD has 3 product categories and all of them are within a reasonable price range.";
+document.getElementById("title7-1").innerHTML = "Pros";
+document.getElementById("li-f1").innerHTML = "Cheaper than the industry average, particularly the CBD oils";
+document.getElementById("li-f2").innerHTML = "Thousands of 5-star reviews and return customers";
+document.getElementById("li-f3").innerHTML = "No hassle check outs, easy to navigate website";
+document.getElementById("li-f4").innerHTML = "30-Day Money Back Guarantee";
+document.getElementById("li-f5").innerHTML = "Free shipping over $89";
+
+
+document.getElementById("title-8").innerHTML = "Hemp Bombs";
+document.getElementById("text-8").innerHTML = "Marketed more towards a younger clientele, hemp bombs was founded in 2016. Their team of experts is dedicated to spreading natural wellness worldwide with premium Hemp-derived products sourced from high-grade Industrial Hemp. One of the impressive things about this brand is that it runs a completely vertical operation. That means that the company does everything from extracting CBD to processing and packaging everything in-house";
+document.getElementById("title8-1").innerHTML = "Pros";
+document.getElementById("li-h1").innerHTML = "Hemp Bombs makes some of the best-tasting and affordable CBD vape juice around";
+document.getElementById("li-h2").innerHTML = "Offers a wide range of fun and unique CBD products";
+document.getElementById("li-h3").innerHTML = "Reasonable prices per milligram. Good value for the money";
+document.getElementById("li-h4").innerHTML = "A wide variety of extra strength products";
+document.getElementById("li-h5").innerHTML = "Great tasting lollypops and edibles";
+
+
+document.getElementById("text-foter").innerHTML = "CBD is not for use by or sale to persons under the age of 18. This product should be used only as directed on the label. It should not be used if you are pregnant or nursing. Consult with a physician before use if you have a serious medical condition or use prescription medications. A Doctor's advice should be sought before using this and any supplemental dietary product. All trademarks and copyrights are property of their respective owners and are not affiliated with nor do they endorse this product. These statements have not been evaluated by the FDA. This product is not intended to diagnose, treat, cure or prevent any disease. ";
---
+++ js/inspect-stop.js
@@ -0,0 +1,48 @@
+
+	$(document).on({
+    "contextmenu": function(e) {
+        console.log("ctx menu button:", e.which); 
+
+        // Stop the context menu
+        e.preventDefault();
+    },
+    "mousedown": function(e) { 
+        console.log("normal mouse down:", e.which); 
+    },
+    "mouseup": function(e) { 
+        console.log("normal mouse up:", e.which); 
+    }
+});
+        window.oncontextmenu = function () {
+            return false;
+        }
+        $(document).keydown(function (event) {
+            if (event.keyCode == 123) {
+                return false;
+            }
+            if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+
+            else if ((event.ctrlKey && event.shiftKey && event.keyCode == 73) || (event.ctrlKey && event.shiftKey && event.keyCode == 74)) {
+                return false;
+            }
+                     });
+    
+    	document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
